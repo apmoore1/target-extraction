12c152a68f5417e2ba97b3725f4a66fecf06997c
Andrew Moore <andrew.p.moore94@gmail.com>
2019-06-08 20:28:18 +0100

    SemEval dataset parser

--- target_extraction/dataset_parsers.py
+++ target_extraction/dataset_parsers.py
@@ -7,8 +7,94 @@ Functions:
 1. semeval_2014
 '''
 from pathlib import Path
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import ParseError
+from typing import List, Union
 
-from target_extraction.data_types import TargetTextCollection
+from target_extraction.data_types import TargetTextCollection, TargetText
+from target_extraction.data_types_util import Span
+
+def _semeval_extract_data(sentence_tree: Element, conflict: bool
+                          ) -> TargetTextCollection:
+    '''
+    :param sentence_tree: The root element of the XML tree that has come 
+                          from a SemEval XML formatted XML File.
+    :param conflict: Whether or not to include targets or categories that 
+                     have the `conflict` sentiment value. True is to include 
+                     conflict targets and categories.
+    :returns: The SemEval data formatted into a 
+              `target_extraction.data_types.TargetTextCollection` object.
+    '''
+    target_text_collection = TargetTextCollection()
+    for sentence in sentence_tree:
+        text_id = sentence.attrib['id']
+        
+        targets: List[str] = []
+        target_sentiments: List[Union[str, int]] = []
+        spans: List[Span] = []
+
+        category_sentiments: List[Union[str, int]] = []
+        categories: List[str] = [] 
+
+        for data in sentence:
+            if data.tag == 'text':
+                text = data.text
+            elif data.tag == 'aspectTerms':
+                for target in data:
+                    # If it is a conflict sentiment and conflict argument True 
+                    # skip this target
+                    target_sentiment = target.attrib['polarity']
+                    if conflict and target_sentiment == 'conflict':
+                        continue
+                    targets.append(target.attrib['term'])
+                    target_sentiments.append(target_sentiment)
+                    span_from = int(target.attrib['from'])
+                    span_to = int(target.attrib['to'])
+                    spans.append(Span(span_from, span_to))
+            elif data.tag == 'aspectCategories':
+                for category in data:
+                    # If it is a conflict sentiment and conflict argument True 
+                    # skip this category
+                    category_sentiment = category.attrib['polarity']
+                    if conflict and category_sentiment == 'conflict':
+                        continue
+                    categories.append(category.attrib['category'])
+                    category_sentiments.append(category.attrib['polarity'])
+        target_text_kwargs = {'targets': targets, 'spans': spans, 'text_id': text_id,
+                              'target_sentiments': target_sentiments,
+                              'categories': categories, 'text': text, 
+                              'category_sentiments': category_sentiments}
+        for key in target_text_kwargs:
+            if not target_text_kwargs[key]:
+                target_text_kwargs[key] = None
+        target_text = TargetText(**target_text_kwargs)
+        target_text_collection.add(target_text)
+    return target_text_collection
 
 def semeval_2014(data_fp: Path, conflict: bool) -> TargetTextCollection:
-    return TargetTextCollection()
+    '''
+
+    The sentiment labels are the following: 1. negative, 2. neutral, 
+    3. positive, and 4. conflict. conflict will not appear if the argument 
+    `conflict` is False.
+
+    :param data_fp: Path to the SemEval 2014 formatted file.
+    :param conflict: Whether or not to include targets or categories that 
+                     have the `conflict` sentiment value. True is to include 
+                     conflict targets and categories.
+    :returns: The SemEval 2014 data formatted into a 
+              `target_extraction.data_types.TargetTextCollection` object.
+    :raises SyntaxError: If the File passed is detected as not a SemEval 
+                         formatted file. 
+    :raises `xml.etree.ElementTree.ParseError`: If the File passed is 
+                                                not formatted correctly e.g. 
+                                                mismatched tags
+    '''
+
+    tree = ET.parse(data_fp)
+    sentences = tree.getroot()
+    if sentences.tag != 'sentences':
+        raise SyntaxError('The root of all semeval xml files should '
+                          f'be sentences and not {sentences.tag}')
+    return _semeval_extract_data(sentences, conflict)
--- tests/dataset_parsers/semeval_2014_test.py
+++ tests/dataset_parsers/semeval_2014_test.py
@@ -1,19 +1,46 @@
 from pathlib import Path
 from typing import Dict, List, Any
+from xml.etree.ElementTree import ParseError
 
 import pytest
 
 from target_extraction.dataset_parsers import semeval_2014
 from target_extraction.data_types_util import Span
 
-class Semeval2014:
+class TestSemeval2014:
 
-    DATA_PATH_DIR = Path(__file__, '..', '..', 'data', 'parsing', 'semeval_14')
+    DATA_PATH_DIR = Path(__file__, '..', '..', 'data', 'parsing', 'semeval_14').resolve()
 
     def _target_answer(self, conflict: bool) -> Dict[str, Dict[str, Any]]:
         answer_0 = {'text_id': '3121', 'text': "The staff were ok", 
                     'targets': ['staff'], 'spans': [Span(4, 9)],
-                    'sentiments': ['neutral']}
+                    'target_sentiments': ['neutral'], 
+                    'category_sentiments': ['neutral'],
+                    'categories': ['service']}
+        answer_1 = {'text_id': '2777', 'text': "The day was ok but the food was great", 
+                    'targets': ['food'], 'spans': [Span(23, 27)],
+                    'target_sentiments': ['positive'], 
+                    'category_sentiments': ['positive', 'negative'],
+                    'categories': ['food', 'anecdotes/miscellaneous']}
+        answer_2 = {'text_id': '2534', 'text': "I had a horrible waiter today", 
+                    'category_sentiments': ['negative'],
+                    'categories': ['service']}
+        answer_3 = {'text_id': '2634', 'text': "Nothing really happened today",
+                    'targets': None, 'spans': None, 'target_sentiments': None}
+        answer_4 = {'text_id': '1793', 'text': "The drinks were neither good or bad bu the check in was awful", 
+                    'targets': ['drinks', 'check'], 'spans': [Span(4, 10), Span(43, 48)],
+                    'target_sentiments': ['conflict', 'negative'], 
+                    'category_sentiments': ['conflict'],
+                    'categories': ['service']}
+        if conflict:
+            answer_4['target_sentiments'] = ['negative']
+            answer_4['spans'] = [Span(43, 48)]
+            answer_4['targets'] = ['check']
+            answer_4['category_sentiments'] = None
+            answer_4['categories'] = None
+        return {'3121': answer_0, '2777': answer_1, '2534': answer_2,
+                '2634': answer_3, '1793': answer_4}
+        
 
     @pytest.mark.parametrize("conflict", (True, False))
     def test_read_from_file(self, conflict: bool):
@@ -24,16 +51,29 @@ class Semeval2014:
 
         _ids = ['3121', '2777', '2534', '2634', '1793']
         assert list(target_text_collection.keys()) == _ids
-
-
+        true_answers = self._target_answer(conflict=conflict)
+        for answer_key, answer in true_answers.items():
+            test_answer = target_text_collection[answer_key]
+            for key in answer:
+                assert answer[key] == test_answer[key]
 
     @pytest.mark.parametrize("conflict", (True, False))
     def test_unreadable_file(self, conflict: bool):
-        # Test that it will raise a SyntaxError if a file is not of correct
-        # format
+        # Test that it will raise a ParseError is not formatted correctly e.g. 
+        # Contains mismatched tags
 
         unreadable_fp = Path(self.DATA_PATH_DIR, 
                              'unpassable_semeval_14_example.xml')
+        with pytest.raises(ParseError):
+            semeval_2014(unreadable_fp, conflict)
+
+    @pytest.mark.parametrize("conflict", (True, False))
+    def test_not_semeval_file(self, conflict: bool):
+        # Test that it will raise a SyntaxError as the file does not follow 
+        # SemEval format.
+
+        unreadable_fp = Path(self.DATA_PATH_DIR, 
+                             'not_semeval_14_example.xml')
         with pytest.raises(SyntaxError):
             semeval_2014(unreadable_fp, conflict)
 
