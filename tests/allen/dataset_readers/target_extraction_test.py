ddc521ea2562e1b2714ba9d738d8fe50ec0f270f
Andrew Moore <andrew.p.moore94@gmail.com>
2019-06-19 19:37:48 +0100

    Created dataset reader

--- tests/allen/dataset_readers/target_extraction_test.py
+++ tests/allen/dataset_readers/target_extraction_test.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from allennlp.common.checks import ConfigurationError
 from allennlp.common.util import ensure_list
 import pytest
 
@@ -11,45 +12,73 @@ class TestTargetExtractionDatasetReader():
     def test_read_from_file(self, lazy: bool, pos_tags: bool):
         reader = TargetExtractionDatasetReader(lazy=lazy, pos_tags=pos_tags)
         
-        data_dir = Path(__file__, '..', '..', 'data', 'allen', 
+        data_dir = Path(__file__, '..', '..', '..', 'data', 'allen', 
                         'dataset_readers', 'target_extraction').resolve()
-        pos_tagged_fp = Path(data_dir, 'pos_sequence.json')
-        
-        instances = ensure_list(reader.read(str(pos_tagged_fp)))
 
         instance1 = {"tokens": ["The", "laptop", "case", "was", "great", "and", 
                                 "cover", "was", "rubbish"],
                      "pos_tags": ["DET", "NOUN", "NOUN", "AUX", "ADJ", "CCONJ", 
                                   "NOUN", "AUX", "ADJ"],
                      "labels": ["O", "B", "I", "O", "O", "O", "O", "O", "O"]}
-        instance2 = {"text": ["The", "So", "called", "desktop", "Runs", "to",
-                              "badly"],
-                     "targets": [["Runs"]],
-                     "sentiments": [sentiment_mapper[-1]]}
-        instance3 = {"text": ["errrr", "when", "I", "order", "I", "did", "go",
-                              "full", "scale", "for", "the", "noting", "or",
-                              "full", "trackpad", "I", "wanted", "something",
-                              "for", "basics", "of", "being", "easy", "to",
-                              "move", "It", "."],
-                     "targets": [["noting"], ["move", "It"], ["trackpad"]],
-                     "sentiments": [sentiment_mapper[0], sentiment_mapper[1], 
-                                    sentiment_mapper[0]]}
+        instance2 = {"tokens": ["Another", "day", "at", "the", "office"],
+                     "pos_tags": ["DET", "NOUN", "ADP", "DET", "NOUN"],
+                     "labels": ["O", "O", "O", "O", "O"]}
+        instance3 = {"tokens": ["The", "laptop", "case", "was", "great", "and", 
+                                "cover", "was", "rubbish"],
+                     "pos_tags": ["DET", "NOUN", "NOUN", "AUX", "ADJ", "CCONJ", 
+                                  "NOUN", "AUX", "ADJ"],
+                     "labels": ["O", "B", "I", "O", "O", "O", "B", "O", "O"]}
+
+        # POS tagged data 
+        pos_tagged_fp = Path(data_dir, 'pos_sequence.json')
+        instances = ensure_list(reader.read(str(pos_tagged_fp)))
 
         assert len(instances) == 3
         fields = instances[0].fields
-        assert [t.text for t in fields["text"]] == instance1["text"]
-        for index, target_field in enumerate(fields['targets']):
-            assert [t.text for t in target_field] == instance1["targets"][index]
-        assert fields['labels'].labels == instance1["sentiments"]
+        assert [t.text for t in fields["tokens"]] == instance1["tokens"]
+        if pos_tags:
+            assert fields['pos_tags'].labels == instance1["pos_tags"]
+        else:
+            assert 'pos_tags' not in fields
+        assert fields['labels'].labels == instance1["labels"]
         
         fields = instances[1].fields
-        assert [t.text for t in fields["text"]] == instance2["text"]
-        for index, target_field in enumerate(fields['targets']):
-            assert [t.text for t in target_field] == instance2["targets"][index]
-        assert fields['labels'].labels == instance2["sentiments"]
+        assert [t.text for t in fields["tokens"]] == instance2["tokens"]
+        if pos_tags:
+            assert fields['pos_tags'].labels == instance2["pos_tags"]
+        else:
+            assert 'pos_tags' not in fields
+        assert fields['labels'].labels == instance2["labels"]
 
         fields = instances[2].fields
-        assert [t.text for t in fields["text"]] == instance3["text"]
-        for index, target_field in enumerate(fields['targets']):
-            assert [t.text for t in target_field] == instance3["targets"][index]
-        assert fields['labels'].labels == instance3["sentiments"]
+        assert [t.text for t in fields["tokens"]] == instance3["tokens"]
+        if pos_tags:
+            assert fields['pos_tags'].labels == instance3["pos_tags"]
+        else:
+            assert 'pos_tags' not in fields
+        assert fields['labels'].labels == instance3["labels"]
+
+        # Non-POS tagged version
+        non_pos_tagged_fp = Path(data_dir, 'non_pos_sequence.json')
+
+        if not pos_tags:
+            instances = ensure_list(reader.read(str(non_pos_tagged_fp)))
+
+            assert len(instances) == 3
+            fields = instances[0].fields
+            assert [t.text for t in fields["tokens"]] == instance1["tokens"]
+            assert 'pos_tags' not in fields
+            assert fields['labels'].labels == instance1["labels"]
+            
+            fields = instances[1].fields
+            assert [t.text for t in fields["tokens"]] == instance2["tokens"]
+            assert 'pos_tags' not in fields
+            assert fields['labels'].labels == instance2["labels"]
+
+            fields = instances[2].fields
+            assert [t.text for t in fields["tokens"]] == instance3["tokens"]
+            assert 'pos_tags' not in fields
+            assert fields['labels'].labels == instance3["labels"]
+        else:
+            with pytest.raises(ConfigurationError):
+                ensure_list(reader.read(str(non_pos_tagged_fp)))
