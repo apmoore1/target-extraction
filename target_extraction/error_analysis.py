7a1a929067b70c6c66d67de4df7b42c28c7c8b68
Andrew Moore <andrew.p.moore94@gmail.com>
2019-07-26 10:02:41 +0100

    Code re-factored

--- target_extraction/error_analysis.py
+++ target_extraction/error_analysis.py
@@ -3,9 +3,9 @@ This module is dedictaed to creating new TargetTextCollections that are
 subsamples of the original(s) that will allow the user to analysis the 
 data with respect to some certain property.
 '''
-from typing import List
+from typing import List, Callable, Dict
 
-from target_extraction.data_types import TargetTextCollection
+from target_extraction.data_types import TargetTextCollection, TargetText
 
 def count_error_key_occurence(dataset: TargetTextCollection, 
                               error_key: str) -> int:
@@ -27,6 +27,38 @@ def count_error_key_occurence(dataset: TargetTextCollection,
         count += sum(target_data[error_key])
     return count
 
+def _pre_post_subsampling(test_dataset: TargetTextCollection, 
+                          train_dataset: TargetTextCollection, 
+                          lower: bool, error_key: str,
+                          error_func: Callable[[TargetText, Dict[str, List[str]], 
+                                                Dict[str, List[str]]], bool]
+                          ) -> TargetTextCollection:
+    train_target_sentiments = train_dataset.target_sentiments(lower=lower, 
+                                                              unique_sentiment=True)
+    test_target_sentiments = test_dataset.target_sentiments(lower=lower, 
+                                                            unique_sentiment=True)
+
+    for target_data in test_dataset.values():
+        test_targets = target_data['targets']
+        error_values: List[int] = []
+        if test_targets is None:
+            target_data[error_key] = error_values
+            continue
+        for target in test_targets:
+            if lower:
+                target = target.lower()
+            if error_func(target, train_target_sentiments, 
+                          test_target_sentiments):
+                error_values.append(1)
+                continue
+            error_values.append(0)
+        assert_err_msg = 'This should not occur as the number of targets in '\
+                         f'this TargetText object {target_data} should equal '\
+                         f'the number of same_one_value integer list {error_values}'
+        assert len(test_targets) == len(error_values), assert_err_msg
+        target_data[error_key] = error_values
+    return test_dataset
+
 def same_one_sentiment(test_dataset: TargetTextCollection, 
                        train_dataset: TargetTextCollection, 
                        lower: bool = True) -> TargetTextCollection:
@@ -47,32 +79,62 @@ def same_one_sentiment(test_dataset: TargetTextCollection,
     :returns: The test dataset but with each TargetText object containing a 
               `same_one_sentiment` key and associated list of values.
     '''
-    train_target_sentiments = train_dataset.target_sentiments(lower=lower, 
-                                                              unique_sentiment=True)
-    test_target_sentiments = test_dataset.target_sentiments(lower=lower, 
-                                                            unique_sentiment=True)
-    for target_data in test_dataset.values():
-        test_targets = target_data['targets']
-        same_one_values: List[int] = []
-        if test_targets is None:
-            target_data['same_one_sentiment'] = same_one_values
-            continue
-        for target in test_targets:
-            if lower:
-                target = target.lower()
-            if (target in train_target_sentiments and 
-                target in test_target_sentiments):
-                train_sentiments = train_target_sentiments[target]
-                test_sentiments = test_target_sentiments[target]
-                if (len(train_sentiments) == 1 and 
-                    len(test_sentiments) == 1):
-                    if train_sentiments == test_sentiments:
-                        same_one_values.append(1)
-                        continue
-            same_one_values.append(0)
-        assert_err_msg = 'This should not occur as the number of targets in '\
-                         f'this TargetText object {target_data} should equal '\
-                         f'the number of same_one_value integer list {same_one_values}'
-        assert len(test_targets) == len(same_one_values), assert_err_msg
-        target_data['same_one_sentiment'] = same_one_values
-    return test_dataset
+    
+    def error_func(target: TargetText, 
+                   train_target_sentiments: Dict[str, List[str]],
+                   test_target_sentiments: Dict[str, List[str]]) -> bool:
+        if (target in train_target_sentiments and 
+            target in test_target_sentiments):
+            train_sentiments = train_target_sentiments[target]
+            test_sentiments = test_target_sentiments[target]
+            if (len(train_sentiments) == 1 and len(test_sentiments) == 1):
+                if train_sentiments == test_sentiments:
+                    return True
+        return False    
+    
+    return _pre_post_subsampling(test_dataset, train_dataset, lower, 
+                                 'same_one_sentiment', error_func)
+
+def same_multi_sentiment(test_dataset: TargetTextCollection, 
+                         train_dataset: TargetTextCollection, 
+                         lower: bool = True) -> TargetTextCollection:
+    '''
+    Given a test and train dataset will return the same test dataset but 
+    with an additional key `same_multi_sentiment` for each TargetText object
+    in the test collection. This `same_multi_sentiment` key will contain a list 
+    the same length as the number of targets in that TargetText object with 
+    0's and 1's where a 1 represents the associated target has the same 
+    sentiment labels (more than one sentiment label e.g. positive and negative
+    not just poisitive or not just negative) in the train and test 
+    where as the 0 means it does not.
+
+    :Note: If the target is None then an empty list is returned for that 
+           `same_multi_sentiment` key value.
+
+    :param test_dataset: Test dataset to sub-sample
+    :param train_dataset: Train dataset to reference
+    :param lower: Whether to lower case the target words
+    :returns: The test dataset but with each TargetText object containing a 
+              `same_multi_sentiment` key and associated list of values.
+    '''
+    pass
+    #train_target_sentiments = target_sentiments(train_dataset, lower)
+    #test_target_sentiments = target_sentiments(test_dataset, lower)
+
+    #same_multi_sentiments = set()
+    #for data in test_dataset.data():
+    #    target = data['target']
+    #    if lower:
+    #        target = target.lower()
+    #    if (target in train_target_sentiments and 
+    #        target in test_target_sentiments):
+    #        train_sentiments = train_target_sentiments[target]
+    #        test_sentiments = test_target_sentiments[target]
+    #        if (len(train_sentiments) > 1 and 
+    #            len(test_sentiments) > 1):
+    #            if train_sentiments == test_sentiments:
+    #                same_multi_sentiments.add(target)
+    #same_multi_samples = targets_to_samples(test_dataset, same_multi_sentiments, 
+    #                                        lower)
+    #same_multi_ids = [sample['target_id'] for sample in same_multi_samples]
+    #return same_multi_ids
