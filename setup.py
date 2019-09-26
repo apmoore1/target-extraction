5bfd2c6b719a9edc7f8e44c11c408c33652bc8a8
Andrew Moore <andrew.p.moore94@gmail.com>
2019-09-26 11:08:19 +0100

    Ensure torch is <1.2.0

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(name='target_extraction',
       author_email='andrew.p.moore94@gmail.com',
       license='Apache License 2.0',
       install_requires=[
+          'torch>=0.4.1,<1.2.0',
           'allennlp>=0.8.3,<=0.8.5',
           'spacy>=2.1,<2.2',
-          'torch>=0.4.1',
           'stanfordnlp==0.2.0',
           'twokenize>=1.0.0'
       ],
