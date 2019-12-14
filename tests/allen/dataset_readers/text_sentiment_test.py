8ac52215ed5ab14d7810264d62da5e82a4883854
Andrew Moore <andrew.p.moore94@gmail.com>
2019-12-14 16:49:23 +0000

    Does not depend on the version of Spacy

--- tests/allen/dataset_readers/text_sentiment_test.py
+++ tests/allen/dataset_readers/text_sentiment_test.py
@@ -1,8 +1,11 @@
 import pytest
 from pathlib import Path
+from typing import List, Optional
 
 from allennlp.common.util import ensure_list
 from allennlp.common.testing import AllenNlpTestCase
+from allennlp.data.tokenizers.sentence_splitter import SpacySentenceSplitter
+from allennlp.common.util import get_spacy_model
 
 from target_extraction.allen.dataset_readers import TextSentimentReader
 
@@ -225,11 +228,14 @@ class TestTextSentimentReader:
         assert [t.text for t in fields["tokens"].tokens] == instance3["tokens"]
         assert fields["label"].label == instance3["label"]
 
+    @pytest.mark.parametrize("max_sequence_length", (None, 5))
     @pytest.mark.parametrize("label_name", ('label', 'text_sentiment'))
     @pytest.mark.parametrize("lazy", (True, False))
-    def test_read_from_file_ag_news_corpus_and_segments_sentences_properly(self, lazy: bool, label_name: str):
+    def test_read_from_file_ag_news_corpus_and_segments_sentences_properly(self, lazy: bool, label_name: str, 
+                                                                           max_sequence_length: Optional[int]):
         reader = TextSentimentReader(lazy=lazy, segment_sentences=True, 
-                                     label_name=label_name)
+                                     label_name=label_name, 
+                                     max_sequence_length=max_sequence_length)
         ag_path = (
             AllenNlpTestCase.FIXTURES_ROOT
             / "data"
@@ -241,72 +247,34 @@ class TestTextSentimentReader:
         instances = reader.read(ag_path)
         instances = ensure_list(instances)
 
-        instance1 = {
-            "tokens": [
-                ["Memphis", "Rout", "Still", "Stings", "for", "No", ".", "14", "Louisville", ";"],
-                ["Coach", "Petrino", "Vows", "to", "Have"],
-                ["Team", "Better", "Prepared", "."],
-                ["NASHVILLE", ","],
-                ["Tenn.", "Nov", "3", ",", "2004", "-", "Louisville", "#", "39;s",
-                 "30-point", "loss", "at", "home", 'to', 'Memphis', 'last', 
-                 'season', 'is', 'still', 'a', 'painful', 'memory', 'for', 'the', 
-                 'Cardinals', '.'],
-            ],
-            "label": "2",
-        }
-        instance2 = {
-            "tokens": [['AP', '-', 'Eli', 'Manning', 'has', 'replaced', 'Kurt', 
-                        'Warner', 'as', 'the', 'New', 'York', 'Giants', "'", 
-                        'starting', 'quarterback', '.']],
-            "label": "2",
-        }
-        instance3 = {
-            "tokens": [
-                [
-                    "A",
-                    "conference",
-                    "dedicated",
-                    "to",
-                    "online",
-                    "journalism",
-                    "explores",
-                    "the",
-                    "effect",
-                    "blogs",
-                    "have",
-                    "on",
-                    "news",
-                    "reporting",
-                    ".",
-                ],
-                [
-                    "Some",
-                    "say",
-                    "they",
-                    "draw",
-                    "attention",
-                    "to",
-                    "under",
-                    "-",
-                    "reported",
-                    "stories",
-                    ".",
-                ],
-                [
-                    "Others",
-                    "struggle",
-                    "to",
-                    "establish",
-                    "the",
-                    "credibility",
-                    "enjoyed",
-                    "by",
-                    "professionals",
-                    ".",
-                ],
-            ],
-            "label": "4",
-        }
+        splitter = SpacySentenceSplitter()
+        spacy_tokenizer = get_spacy_model("en_core_web_sm", False, False, False)
+        
+        text1 = ("Memphis Rout Still Stings for No. 14 Louisville; Coach "
+                 "Petrino Vows to Have Team Better Prepared. NASHVILLE, "
+                 "Tenn. Nov 3, 2004 - Louisville #39;s 30-point loss "
+                 "at home to Memphis last season is still a painful memory "
+                 "for the Cardinals.")
+        instance1 = {"text": text1, "label": "2"}
+        text2 = ("AP - Eli Manning has replaced Kurt Warner as the New York"
+                 " Giants' starting quarterback.")
+        instance2 = {"text": text2, "label": "2"}
+        text3 = ("A conference dedicated to online journalism explores the "
+                 "effect blogs have on news reporting. Some say they draw "
+                 "attention to under-reported stories. Others struggle to "
+                 "establish the credibility enjoyed by professionals.")
+        instance3 = {"text": text3, "label": "4"}
+        
+        for instance in [instance1, instance2, instance3]:
+            sentences = splitter.split_sentences(instance['text'])
+            tokenized_sentences: List[List[str]] = []
+            for sentence in sentences:
+                tokens = [token.text for token in spacy_tokenizer(sentence)]
+                if max_sequence_length:
+                    tokens = tokens[:max_sequence_length]
+                tokenized_sentences.append(tokens)
+            instance["tokens"] = tokenized_sentences
+
 
         assert len(instances) == 3
         fields = instances[0].fields
