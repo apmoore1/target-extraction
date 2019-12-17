5e729878ad584206993030d757f60eaf52ac5097
Andrew Moore <andrew.p.moore94@gmail.com>
2019-12-17 13:54:41 +0000

    Now add metadata to a metric dataframe

--- target_extraction/analysis/util.py
+++ target_extraction/analysis/util.py
@@ -1,8 +1,10 @@
-from typing import Optional, Callable, Union, List
+from collections import defaultdict
+from typing import Optional, Callable, Union, List, Dict, Any
 
 import pandas as pd
+import numpy as np
 
-from target_extraction.data_types import TargetTextCollection
+from target_extraction.data_types import TargetTextCollection, TargetText
 
 def metric_df(target_collection: TargetTextCollection, 
               metric_function: Callable[[TargetTextCollection, str, str, bool, bool, Optional[int]], 
@@ -56,3 +58,72 @@ def metric_df(target_collection: TargetTextCollection,
     df_dict = {f'{metric_name}': df_metric_values, 
                'prediction key': df_predicted_keys}
     return pd.DataFrame(df_dict)
+
+def add_metadata_to_df(df: pd.DataFrame, target_collection: TargetTextCollection, 
+                       metadata_prediction_key: str, 
+                       metadata_keys: Optional[List[str]] = None
+                       ) -> pd.DataFrame:
+    '''
+    :param df: A DataFrame that contains at least one column named `prediction key`
+               of which the values in `prediction key` releate to the keys within
+               TargetTextCollection that store the related to predicted values
+    :param target_collection: The collection that stores `prediction key` and 
+                              the metadata within `target_collection.metadata`
+    :param metadata_prediction_key: The key that stores all of the metadata 
+                                    associated to the `prediction key` values 
+                                    within `target_collection.metadata`
+    :param metadata_keys: If not None will only add the metadata keys that relate 
+                          to the `prediction key` that are stated in this list of 
+                          Strings else will add all.
+    :returns: The `df` dataframe but with new columns that are the names of the 
+              metadata fields with the values being the values from those metadata
+              fields that relate to the `prediction key` value.
+    :raises KeyError: If any of the `prediction key` values are not keys within 
+                      the TargetTextCollection targets.
+    :raises KeyError: If any of the prediction key values in the dataframe are not 
+                      in the `target_collection` metadata.
+    '''
+    metadata = target_collection.metadata
+    if metadata is None:
+        return df
+    if metadata_prediction_key not in metadata:
+        return df
+    prediction_keys = df['prediction key'].unique()
+    for prediction_key in prediction_keys:
+        for target_text in target_collection.values():
+            target_text: TargetText
+            target_text._key_error(prediction_key)
+    
+    
+    metadata_prediction_data: Dict[str, Any] = metadata[metadata_prediction_key]
+    prediction_key_metadata: Dict[str, Dict[str, Any]] = defaultdict(dict)
+    metadata_column_names = set()
+    
+    for prediction_key in prediction_keys:
+        if prediction_key not in metadata_prediction_data:
+            raise KeyError(f'The following prediction key {prediction_key} is '
+                           f'not in the metadata {metadata_prediction_key} '
+                           f'dictionary {metadata_prediction_data}')
+        for column_name, column_value in metadata_prediction_data[prediction_key].items():
+            # Skip metadata columns that are to be skipped as stated in the
+            # arguments
+            if metadata_keys is not None:
+                if column_name not in metadata_keys:
+                    continue
+            prediction_key_metadata[prediction_key][column_name] = column_value
+            metadata_column_names.add(column_name)
+    # default values for the metadata
+    for metadata_column in metadata_column_names:
+        df[f'{metadata_column}'] = np.nan
+    # Add the metadata to the dataframe
+    for prediction_key, name_value in prediction_key_metadata.items():
+        for column_name, column_value in name_value.items():
+            df.loc[df['prediction key']==prediction_key, column_name] = column_value
+    return df
+    
+
+    
+            
+            
+
+    
--- tests/analysis/analysis_util_test.py
+++ tests/analysis/analysis_util_test.py
@@ -1,6 +1,9 @@
 from typing import List, Tuple
+import math
 
 import pytest
+import numpy as np
+import pandas as pd
 
 from target_extraction.analysis import util, sentiment_metrics
 from target_extraction.data_types import TargetTextCollection, TargetText
@@ -59,3 +62,91 @@ def test_metric_df(metric_function_name: str, metric_name: str):
         test_model_scores = test_model_scores.to_list()
         assert 1 == len(test_model_scores)
         assert true_model_scores == test_model_scores[0]
+
+def test_add_metadata_to_df():
+    model_1_collection = passable_example_multiple_preds('true_sentiments', 'model_1')
+    model_2_collection = passable_example_multiple_preds('true_sentiments', 'model_2')
+    combined_collection = TargetTextCollection()
+    for key, value in model_1_collection.items():
+        combined_collection.add(value)
+        combined_collection[key]['model_2'] = model_2_collection[key]['model_2']
+    # get test metric_df
+    combined_collection.metadata = None
+    metric_df = util.metric_df(combined_collection, sentiment_metrics.accuracy, 
+                               'true_sentiments', 
+                               predicted_sentiment_keys=['model_1', 'model_2'], 
+                               average=False, array_scores=True, metric_name='metric')
+    # Test the case where the TargetTextCollection has no metadata, should 
+    # just return the dataframe as is without change
+    test_df = util.add_metadata_to_df(metric_df.copy(deep=True), combined_collection, 
+                                      'non-existing-key')
+    assert metric_df.equals(test_df)
+    assert combined_collection.metadata is None
+    # Testing the case where the metadata is not None but does not contain the 
+    # `metadata_prediction_key` = `non-existing-key`
+    combined_collection.name = 'combined collection'
+    assert combined_collection.metadata is not None
+    test_df = util.add_metadata_to_df(metric_df.copy(deep=True), combined_collection, 
+                                      'non-existing-key')
+    assert metric_df.equals(test_df)
+
+    # Test the normal cases where we are adding metadata
+    key_metadata_normal = {'model_1': {'embedding': True, 'value': '10'}, 
+                           'model_2': {'embedding': False, 'value': '5'}}
+    key_metadata_alt = {'model_1': {'embedding': True, 'value': '10'}, 
+                        'model_2': {'embedding': False, 'value': '5'},
+                        'model_3': {'embedding': 'low', 'value': '12'}}
+    key_metadata_diff = {'model_1': {'embedding': True, 'value': '10', 'special': 12}, 
+                         'model_2': {'embedding': False, 'value': '5', 'diff': 30.0}}
+    key_metadataer = [key_metadata_normal, key_metadata_alt, key_metadata_diff]
+    for key_metadata in key_metadataer:
+        combined_collection.metadata['non-existing-key'] = key_metadata
+        if 'special' in key_metadata['model_1']:
+            test_df = util.add_metadata_to_df(metric_df.copy(deep=True), combined_collection, 
+                                              'non-existing-key', 
+                                              metadata_keys=['embedding', 'value'])
+        else:
+            test_df = util.add_metadata_to_df(metric_df.copy(deep=True), combined_collection, 
+                                              'non-existing-key')
+        assert not metric_df.equals(test_df)
+        assert (4, 4) == test_df.shape
+        assert [True, True] == test_df.loc[test_df['prediction key']=='model_1']['embedding'].to_list()
+        assert [False, False] == test_df.loc[test_df['prediction key']=='model_2']['embedding'].to_list()
+        assert ['10', '10'] == test_df.loc[test_df['prediction key']=='model_1']['value'].to_list()
+        assert ['5', '5'] == test_df.loc[test_df['prediction key']=='model_2']['value'].to_list()
+    # Test the case where some of the metadata exists for some of the models in
+    # the collection but not all of them
+    combined_collection.metadata['non-existing-key'] = key_metadata_diff
+    test_df = util.add_metadata_to_df(metric_df.copy(deep=True), combined_collection, 
+                                      'non-existing-key')
+    assert not metric_df.equals(test_df)
+    assert (4, 6) == test_df.shape
+    assert [True, True] == test_df.loc[test_df['prediction key']=='model_1']['embedding'].to_list()
+    assert [False, False] == test_df.loc[test_df['prediction key']=='model_2']['embedding'].to_list()
+    assert ['10', '10'] == test_df.loc[test_df['prediction key']=='model_1']['value'].to_list()
+    assert ['5', '5'] == test_df.loc[test_df['prediction key']=='model_2']['value'].to_list()
+    assert [12, 12] == test_df.loc[test_df['prediction key']=='model_1']['special'].to_list()
+    nan_values = test_df.loc[test_df['prediction key']=='model_2']['special'].to_list()
+    assert 2 == len(nan_values)
+    for test_value in nan_values:
+        assert math.isnan(test_value)
+    nan_values = test_df.loc[test_df['prediction key']=='model_1']['diff'].to_list()
+    assert 2 == len(nan_values)
+    for test_value in nan_values:
+        assert math.isnan(test_value)
+    assert [30.0, 30.0] == test_df.loc[test_df['prediction key']=='model_2']['diff'].to_list()
+
+    # Test the KeyError cases
+    # Prediction keys that exist in the metric df but not in the collection
+    metric_copy_df = metric_df.copy(deep=True)
+    alt_metric_df = pd.DataFrame({'prediction key': ['model_3', 'model_3'], 
+                                  'metric': [0.4, 0.5]})
+    metric_copy_df = metric_copy_df.append(alt_metric_df)
+    assert (6, 2) == metric_copy_df.shape
+    with pytest.raises(KeyError):
+        util.add_metadata_to_df(metric_copy_df, combined_collection, 'non-existing-key')
+    # Prediction keys exist in the dataframe and target texts but not in the 
+    # metadata
+    combined_collection.metadata['non-existing-key'] = {'model_1': {'embedding': True, 'value': '10'}}
+    with pytest.raises(KeyError):
+        util.add_metadata_to_df(metric_df.copy(deep=True), combined_collection, 'non-existing-key')
