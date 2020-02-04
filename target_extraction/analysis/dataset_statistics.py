5f306a9a6cbce83d78aeff63bf53192781b84cd4
Andrew Moore <andrew.p.moore94@gmail.com>
2020-02-04 09:58:07 +0000

    Dataset stats

--- target_extraction/analysis/dataset_statistics.py
+++ target_extraction/analysis/dataset_statistics.py
@@ -3,14 +3,18 @@ This module allows TargetTextCollection objects to be analysed and report
 overall statistics.
 '''
 from collections import defaultdict
-from typing import Dict, Any, List, Union
+from typing import Dict, Any, List, Union, Callable
 
 from target_extraction.data_types import TargetTextCollection
+from target_extraction.tokenizers import spacy_tokenizer
 
 def get_sentiment_counts(collection: TargetTextCollection,
+                         sentiment_key: str,
                          normalised: bool = True) -> Dict[str, float]:
     '''
     :param collection: The collection containing the sentiment data
+    :param sentiment_key: The key in each TargetText within the collection that 
+                          contains the True sentiment value.
     :param normalised: Whether to normalise the values in the dictionary 
                        by the number of targets in the collection.
     :returns: A dictionary where keys are sentiment values and the keys 
@@ -18,8 +22,8 @@ def get_sentiment_counts(collection: TargetTextCollection,
     '''
     sentiment_count = defaultdict(lambda: 0)
     for target_text in collection.values():
-        if target_text['target_sentiments'] is not None:
-            for sentiment_value in target_text['target_sentiments']:
+        if target_text[sentiment_key] is not None:
+            for sentiment_value in target_text[sentiment_key]:
                 sentiment_count[sentiment_value] += 1
     number_targets = collection.number_targets()
     assert number_targets == sum(sentiment_count.values())
@@ -48,12 +52,69 @@ def average_target_per_sentences(collection: TargetTextCollection,
         number_sentences = len(collection)
     return number_targets / float(number_sentences)
 
-def dataset_target_statistics(collections: List[TargetTextCollection],
-                              lower_target: bool = True
-                              ) -> List[Dict[str, Union[str,int,float]]]:
+def tokens_per_target(collection: TargetTextCollection,
+                      target_key: str, 
+                      tokeniser: Callable[[str], List[str]],
+                      normalise: bool = False,
+                      cumulative_percentage: bool = False) -> Dict[int, int]:
+    '''
+    :param collection: collection to analyse
+    :param target_key: The key within each sample in the collection that contains 
+                       the list of targets to be analysed. This can also be the 
+                       predicted target key, which might be useful for error 
+                       analysis.
+    :param tokenizer: The tokenizer to use to split the target(s) into tokens. See 
+                      for a module of comptabile tokenisers 
+                      :py:mod:`target_extraction.tokenizers`
+    :param normalise: The values are normalised based on the total number of 
+                      targets. (This does not change the return if 
+                      `cumulative_percentage` is True)
+    :param cumulative_percentage: If the return should not be frequency counts of 
+                                  the number of tokens in each target but rather 
+                                  the cumulative percentage of targets with 
+                                  that number of tokens.
+    :returns: The dictionary where keys are the target length based on the number 
+              of tokens in the target and the values are the number of targets 
+              in the dataset that contain that number of tokens (same target can 
+              be counted more than once if it exists in the dataset more then 
+              once). **This is a defaultdict where the value will be 0 if the key 
+              does not exist.**
+    '''
+    lengths = defaultdict(lambda: 0)
+    target_count = collection.target_count(lower=False, target_key=target_key)
+    total_target_count = sum(target_count.values())
+    for target, count in target_count.items():
+        length = len(tokeniser(target))
+        if normalise:
+            count = count / total_target_count
+        lengths[length] += count
+    if cumulative_percentage:
+        lengths = sorted(lengths.items(), key=lambda x: x[0])
+        temp_lengths = {}
+        current_percentage = 0.0
+        for length, count in lengths:
+            percentage = (count/total_target_count) * 100
+            temp_lengths[length] = current_percentage + percentage
+            current_percentage += percentage
+        lengths = temp_lengths
+    return lengths
+
+def dataset_target_extraction_statistics(collections: List[TargetTextCollection],
+                                         lower_target: bool = True,
+                                         target_key: str = 'targets',
+                                         tokeniser: Callable[[str], List[str]]=spacy_tokenizer()
+                                         ) -> List[Dict[str, Union[str,int,float]]]:
     '''
     :param collections: A list of collections
     :param lower_target: Whether to lower case the targets before counting them
+    :param target_key: The key within each sample in each collection that contains 
+                       the list of targets to be analysed. This can also be the 
+                       predicted target key, which might be useful for error 
+                       analysis.
+    :param tokenizer: The tokenizer to use to split the target(s) into tokens. See 
+                      for a module of comptabile tokenisers 
+                      :py:mod:`target_extraction.tokenizers`. This is required 
+                      to give statistics on target length.
     :returns: A list of dictionaries each containing the statistics for the 
               associated collection. Each dictionary will have the following 
               keys:
@@ -66,9 +127,12 @@ def dataset_target_statistics(collections: List[TargetTextCollection],
               6. ATS -- Average Target per Sentence (ATS)
               7. ATS(t) -- ATS but where all sentences in the collection must 
                  contain at least one target.
-              8. Pos % -- Percentage of positive targets
-              9. Neu % -- Percentage of neutral targets
-              10. Neg % -- Percentage of Negative targets
+              8. TL (1) -- Percentage of targets that are length 1 based on the 
+                 number of tokens.
+              9. TL (2) -- Percentage of targets that are length 2 based on the 
+                 number of tokens.
+              10. TL (3+) -- Percentage of targets that are length 3+ based on the 
+                  number of tokens.
     '''
     dataset_stats: List[Dict[str, Union[str,int,float]]] = []
     for collection in collections:
@@ -80,11 +144,49 @@ def dataset_target_statistics(collections: List[TargetTextCollection],
         collection_stats['No. Uniq Targets'] = len(collection.target_count(lower=lower_target))
         collection_stats['ATS'] = average_target_per_sentences(collection, False)
         collection_stats['ATS(t)'] = average_target_per_sentences(collection, True)
-        sentiment_count = get_sentiment_counts(collection, normalised=True)
+        
+        target_lengths = tokens_per_target(collection, target_key, tokeniser, normalise=True)
+        collection_stats['TL (1)'] = target_lengths[1]
+        collection_stats['TL (2)'] = target_lengths[2]
+        three_plus = sum([fraction for token_length, fraction in target_lengths.items() 
+                          if token_length > 2])
+        collection_stats['TL (3+)'] = three_plus
+        dataset_stats.append(collection_stats)
+    return dataset_stats
+
+def dataset_target_sentiment_statistics(collections: List[TargetTextCollection],
+                                        lower_target: bool = True,
+                                        target_key: str = 'targets',
+                                        tokeniser: Callable[[str], List[str]]=spacy_tokenizer(),
+                                        sentiment_key: str = 'target_sentiments'
+                                        ) -> List[Dict[str, Union[str,int,float]]]:
+    '''
+    :param collections: A list of collections
+    :param lower_target: Whether to lower case the targets before counting them
+    :param sentiment_key: The key in each TargetText within each collection that 
+                          contains the True sentiment value.
+    :returns: A list of dictionaries each containing the statistics for the 
+              associated collection. Each dictionary will have the keys from 
+              :py:func:`dataset_target_extraction_statistics` and the following 
+              in addition:
+              1. Pos % -- Percentage of positive targets
+              2. Neu % -- Percentage of neutral targets
+              3. Neg % -- Percentage of Negative targets
+    '''
+    initial_dataset_stats = dataset_target_extraction_statistics(collections, 
+                                                                 lower_target=lower_target, 
+                                                                 target_key=target_key, 
+                                                                 tokeniser=tokeniser)
+    dataset_stats = []
+    for collection, collection_stats in zip(collections, initial_dataset_stats):
+        sentiment_count = get_sentiment_counts(collection, normalised=True, 
+                                               sentiment_key=sentiment_key)
         collection_stats['POS %'] = sentiment_count['positive'] * 100
         collection_stats['NEU %'] = sentiment_count['neutral'] * 100
         collection_stats['NEG %'] = sentiment_count['negative'] * 100
         dataset_stats.append(collection_stats)
     return dataset_stats
+
+
 
 
--- tests/analysis/dataset_statistics_test.py
+++ tests/analysis/dataset_statistics_test.py
@@ -1,14 +1,22 @@
 from pathlib import Path
+import math
+import re
 
 import pytest
 
-from target_extraction.data_types import TargetTextCollection
+from target_extraction.data_types import TargetTextCollection, TargetText
+from target_extraction.data_types_util import Span
 from target_extraction.analysis.dataset_statistics import get_sentiment_counts
 from target_extraction.analysis.dataset_statistics import average_target_per_sentences
-from target_extraction.analysis.dataset_statistics import dataset_target_statistics
+from target_extraction.analysis.dataset_statistics import dataset_target_sentiment_statistics
+from target_extraction.analysis.dataset_statistics import tokens_per_target
+from target_extraction.analysis.dataset_statistics import dataset_target_extraction_statistics
+from target_extraction.tokenizers import whitespace
 
 DATA_DIR = Path(__file__, '..', '..', 'data', 'analysis', 'sentiment_error_analysis').resolve()
 TRAIN_COLLECTION =  TargetTextCollection.load_json(Path(DATA_DIR, 'train_with_blank.json'))
+SENTIMENT_KEY = 'target_sentiments'
+
 ["neutral", "neutral", "neutral", "neutral", "negative"]
 ["negative", "neutral", "neutral", "neutral"]
 ["neutral", "negative", "negative", "negative"]
@@ -20,15 +28,18 @@ def test_get_sentiment_counts():
     total = 19.0
     true_sentiment_counts = dict([('positive', num_pos), ('neutral', num_neu), 
                                   ('negative', num_neg)])
-    sentiment_counts = get_sentiment_counts(TRAIN_COLLECTION, normalised=False)
+    sentiment_counts = get_sentiment_counts(TRAIN_COLLECTION, normalised=False, 
+                                            sentiment_key=SENTIMENT_KEY)
     assert len(true_sentiment_counts) == len(sentiment_counts)
     for sentiment, count in true_sentiment_counts.items():
         assert count == sentiment_counts[sentiment]
 
-    sentiment_counts = get_sentiment_counts(TRAIN_COLLECTION)
+    sentiment_counts = get_sentiment_counts(TRAIN_COLLECTION, SENTIMENT_KEY)
     assert len(true_sentiment_counts) == len(sentiment_counts)
     for sentiment, count in true_sentiment_counts.items():
         assert count/total == sentiment_counts[sentiment] 
+    with pytest.raises(KeyError):
+        get_sentiment_counts(TRAIN_COLLECTION, 'wrong_key')
 
 def test_average_target_per_sentences():
     number_targets = 19.0
@@ -41,60 +52,142 @@ def test_average_target_per_sentences():
     assert true_ats == average_target_per_sentences(TRAIN_COLLECTION, 
                                                     sentence_must_contain_targets=True)
 
-
-["immigration", "patients", "immigration", "NHS", "Tory"]
-["Ed Balls", "@politics_co_uk", "SNPout", "osborne"]
-["BattleForNumber10", "NHS", "immigration", "spending cuts"]
-["Police", "crime", "Conservatives"]
-["police", "crime", "Conservatives"]
-
-["negative", "neutral", "neutral", "neutral"]
-["neutral", "positive", "neutral"]
 @pytest.mark.parametrize("lower", (False, None))
-def test_dataset_target_statistics(lower: bool):
+def test_dataset_target_sentiment_statistics(lower: bool):
     TRAIN_COLLECTION.name = 'train'
     if lower is not None:
-        target_stats = dataset_target_statistics([TRAIN_COLLECTION], 
+        target_stats = dataset_target_sentiment_statistics([TRAIN_COLLECTION], 
                                                  lower_target=lower)
     else:
-        target_stats = dataset_target_statistics([TRAIN_COLLECTION])
-    pos_percent = get_sentiment_counts(TRAIN_COLLECTION)['positive'] * 100
-    neu_percent = get_sentiment_counts(TRAIN_COLLECTION)['neutral'] * 100
-    neg_percent = get_sentiment_counts(TRAIN_COLLECTION)['negative'] * 100
+        target_stats = dataset_target_sentiment_statistics([TRAIN_COLLECTION])
+    pos_percent = get_sentiment_counts(TRAIN_COLLECTION, SENTIMENT_KEY)['positive'] * 100
+    neu_percent = get_sentiment_counts(TRAIN_COLLECTION, SENTIMENT_KEY)['neutral'] * 100
+    neg_percent = get_sentiment_counts(TRAIN_COLLECTION, SENTIMENT_KEY)['negative'] * 100
     true_stats = {'Name': 'train', 'No. Sentences': 6, 'No. Sentences(t)': 5,
                   'No. Targets': 19, 'No. Uniq Targets': 13, 'ATS': 19/6.0,
                   'ATS(t)': 19/5.0, 'POS %': pos_percent, 'NEG %': neg_percent,
-                  'NEU %': neu_percent}
+                  'NEU %': neu_percent, 'TL (1)': 17/19.0, 'TL (2)': 2/19.0,
+                  'TL (3+)': 0.0}
     if lower == False:
         true_stats['No. Uniq Targets'] = 14
+    print(target_stats)
     assert 1 == len(target_stats)
     target_stats = target_stats[0]
     assert len(true_stats) == len(target_stats)
     for stat_name, stat in true_stats.items():
-        assert stat == target_stats[stat_name], stat_name
+        if re.search(r'^TL', stat_name):
+            assert math.isclose(stat, target_stats[stat_name], rel_tol=0.001)
+        else:
+            assert stat == target_stats[stat_name], stat_name
 
     # Multiple collections, where one collection is just the subset of the other
     subcollection = TargetTextCollection(name='sub')
     subcollection.add(TRAIN_COLLECTION["81207500773427072"])
     subcollection.add(TRAIN_COLLECTION["78522643479064576"])
     if lower is not None:
-        target_stats = dataset_target_statistics([subcollection, TRAIN_COLLECTION], 
+        target_stats = dataset_target_sentiment_statistics([subcollection, TRAIN_COLLECTION], 
                                                  lower_target=lower)
     else:
-        target_stats = dataset_target_statistics([subcollection, TRAIN_COLLECTION])
+        target_stats = dataset_target_sentiment_statistics([subcollection, TRAIN_COLLECTION])
     
-    pos_percent = get_sentiment_counts(subcollection)['positive'] * 100
-    neu_percent = get_sentiment_counts(subcollection)['neutral'] * 100
-    neg_percent = get_sentiment_counts(subcollection)['negative'] * 100
+    pos_percent = get_sentiment_counts(subcollection, SENTIMENT_KEY)['positive'] * 100
+    neu_percent = get_sentiment_counts(subcollection, SENTIMENT_KEY)['neutral'] * 100
+    neg_percent = get_sentiment_counts(subcollection, SENTIMENT_KEY)['negative'] * 100
     sub_stats = {'Name': 'sub', 'No. Sentences': 2, 'No. Sentences(t)': 2,
                  'No. Targets': 7, 'No. Uniq Targets': 7, 'ATS': 7/2.0,
                  'ATS(t)': 7/2.0, 'POS %': pos_percent, 'NEG %': neg_percent,
-                 'NEU %': neu_percent}
+                 'NEU %': neu_percent, 'TL (1)': 6/7.0, 'TL (2)': 1/7.0, 
+                 'TL (3+)': 0.0}
     true_stats = [sub_stats, true_stats]
     assert len(true_stats) == len(target_stats)
     for stat_index, stat in enumerate(true_stats):
         test_stat = target_stats[stat_index]
         assert len(stat) == len(test_stat)
         for stat_name, stat_value in stat.items():
-            assert stat_value == test_stat[stat_name], stat_name
+            if re.search(r'^TL', stat_name):
+                assert math.isclose(stat_value, test_stat[stat_name], rel_tol=0.001)
+            else:
+                assert stat_value == test_stat[stat_name], stat_name
 
+
+["immigration", "patients", "immigration", "NHS", "Tory"]
+["Ed Balls", "@politics_co_uk", "SNPout", "osborne"]
+["BattleForNumber10", "NHS", "immigration", "spending cuts"]
+["Police", "crime", "Conservatives"]
+["police", "crime", "Conservatives"]
+def test_tokens_per_target():
+    # standard/normal case
+    length_count = tokens_per_target(TRAIN_COLLECTION, 'targets', whitespace())
+    true_length_count = {1: 17, 2: 2}
+    assert len(length_count) == len(true_length_count)
+    for length, count in true_length_count.items():
+        assert count == length_count[length]
+    # normalise
+    length_frac = tokens_per_target(TRAIN_COLLECTION, 'targets', whitespace(), 
+                                    normalise=True)
+    true_length_frac = {1: 17/19.0, 2: 2/19.0}
+    assert len(length_frac) == len(true_length_frac)
+    for length, frac in true_length_frac.items():
+        assert math.isclose(frac, length_frac[length], rel_tol=0.01)
+    # cumulative percentage
+    length_dist = tokens_per_target(TRAIN_COLLECTION, 'targets', whitespace(), 
+                                    cumulative_percentage=True)
+    true_length_dist = {1: 17/19.0, 2: 1.0}
+    assert len(length_dist) == len(true_length_dist)
+    for length, dist in true_length_dist.items():
+        assert math.isclose(dist * 100, length_dist[length], rel_tol=0.01)
+
+
+@pytest.mark.parametrize("lower", (False, None))
+def test_dataset_target_extraction_statistics(lower: bool):
+    TRAIN_COLLECTION.name = 'train'
+    if lower is not None:
+        target_stats = dataset_target_extraction_statistics([TRAIN_COLLECTION], 
+                                                            lower_target=lower)
+    else:
+        target_stats = dataset_target_extraction_statistics([TRAIN_COLLECTION])
+    true_stats = {'Name': 'train', 'No. Sentences': 6, 'No. Sentences(t)': 5,
+                  'No. Targets': 19, 'No. Uniq Targets': 13, 'ATS': 19/6.0,
+                  'ATS(t)': 19/5.0, 'TL (1)': 17/19.0, 'TL (2)': 2/19.0,
+                  'TL (3+)': 0.0}
+    if lower == False:
+        true_stats['No. Uniq Targets'] = 14
+    assert 1 == len(target_stats)
+    target_stats = target_stats[0]
+    assert len(true_stats) == len(target_stats)
+    for stat_name, stat in true_stats.items():
+        if re.search(r'^TL', stat_name):
+            assert math.isclose(stat, target_stats[stat_name], rel_tol=0.001)
+        else:
+            assert stat == target_stats[stat_name], stat_name
+
+    # Multiple collections, where one collection is just the subset of the other
+    subcollection = TargetTextCollection(name='sub')
+    subcollection.add(TRAIN_COLLECTION["81207500773427072"])
+    subcollection.add(TRAIN_COLLECTION["78522643479064576"])
+    long_target = TargetText(text='some text that contains a long target or two',
+                             spans=[Span(0,14), Span(15, 37)], 
+                             targets=['some text that', 'contains a long target'],
+                             target_sentiments=['positive', 'negative'], 
+                             text_id='100')
+    subcollection.add(long_target)
+    if lower is not None:
+        target_stats = dataset_target_extraction_statistics([subcollection, TRAIN_COLLECTION], 
+                                                            lower_target=lower)
+    else:
+        target_stats = dataset_target_extraction_statistics([subcollection, TRAIN_COLLECTION])
+    
+    sub_stats = {'Name': 'sub', 'No. Sentences': 3, 'No. Sentences(t)': 3,
+                 'No. Targets': 9, 'No. Uniq Targets': 9, 'ATS': 9/3.0,
+                 'ATS(t)': 9/3.0, 'TL (1)': 6/9.0, 'TL (2)': 1/9.0,
+                 'TL (3+)': 2/9.0}
+    true_stats = [sub_stats, true_stats]
+    assert len(true_stats) == len(target_stats)
+    for stat_index, stat in enumerate(true_stats):
+        test_stat = target_stats[stat_index]
+        assert len(stat) == len(test_stat)
+        for stat_name, stat_value in stat.items():
+            if re.search(r'^TL', stat_name):
+                assert math.isclose(stat_value, test_stat[stat_name], rel_tol=0.001)
+            else: 
+                assert stat_value == test_stat[stat_name], stat_name
